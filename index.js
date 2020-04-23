91e3684123a3bf826a5da91d13dae5e71a798c31
Tirta Rachman <tirtawening@gmail.com>
2020-04-23 14:20:26 -0400

    fiat lux

---
+++ index.js
@@ -0,0 +1,8 @@
+let port = process.env.PORT || 80;
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app).listen(port, function () {
+  console.log('Server listening at port: ', port);
+});
+
+app.use(express.static('public'));
